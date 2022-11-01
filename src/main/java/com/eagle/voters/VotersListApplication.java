0c0c42d146aaaf35374ebf2ff946507b10b8cfcb
shivaraj <shivarajpp38@gmail.com>
2022-11-01 11:00:21 +0530

    initial

---
+++ src/main/java/com/eagle/voters/VotersListApplication.java
@@ -0,0 +1,13 @@
+package com.eagle.voters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotersListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotersListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eagle/voters/VotersListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.voters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotersListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
